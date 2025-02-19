c0e52520cc4ac7510da4bea0b436510143d20937
Tom <tom@MacBook-Pro-de-Alejandra.local>
2025-02-19 16:20:33 -0300

    add marginBottom to Card container

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -23,6 +23,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     marginTop: 20,
+    marginBottom: 10,
   },
   promptText: {
     color: Colors.accent500,
