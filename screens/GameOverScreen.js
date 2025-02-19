29d42618c6a2dc336cb3fbf12082895507aa226f
Tom <tom@MacBook-Pro-de-Alejandra.local>
2025-02-19 15:47:25 -0300

    use Title in GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,12 @@
 import { View, Text, StyleSheet } from "react-native";
+
 import Button from "../components/Button";
+import Title from "../components/ui/Title";
 
 export default function GameOverScreen({ number, rounds, startNewGame }) {
   return (
     <View style={styles.container}>
-      <Text>Game Over!</Text>
+      <Title>Game Over!</Title>
       <Text>
         Your phone needed {rounds} rounds to guess the number {number}.
       </Text>
