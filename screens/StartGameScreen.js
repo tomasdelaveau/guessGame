f05f427058730d1197f7eaa6844324d8b2f32b42
Tom <tom@MacBook-Pro-de-Alejandra.local>
2025-03-03 17:19:58 -0300

    adjust marginTop and justifyContent according to device height

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,11 +1,17 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, useWindowDimensions } from "react-native";
 
 import NumberInput from "../components/NumberInput";
 import Title from "../components/ui/Title";
 
 export default function StartGameScreen({ onSet }) {
+  const { width, height } = useWindowDimensions();
+
+  const justify = height > 400 ? "center" : "flex-start";
+  const margin = height > 400 ? 150 : height > 380 ? 50 : 25;
+  const computedStyle = { justifyContent: justify, marginTop: margin };
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, computedStyle]}>
       <Title>Guess my Number</Title>
       <NumberInput onSet={(number) => onSet(number)} />
     </View>
@@ -15,7 +21,5 @@ export default function StartGameScreen({ onSet }) {
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
-    justifyContent: "flex-start",
-    marginTop: 150,
   },
 });
